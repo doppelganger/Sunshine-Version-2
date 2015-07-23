d6d83ce70636690b63f05564836434e41ea9abc2
Rodrigo Toledo <rodrigo.toledo@outlook.com>
2015-07-23 19:55:26 -0300

    Lección 1.06 Finding Views findViewById() Quiz
    NO FUNCIONA

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -69,7 +70,11 @@ public class MainActivity extends ActionBarActivity {
 
             ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
 
+
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ListView listView = (ListView) getActivity().findViewById(R.id.listview_forecast);
+
+            listView.setAdapter(arrayAdapter);
             return rootView;
         }
     }
